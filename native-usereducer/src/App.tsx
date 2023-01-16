d035502076a1d1dc7e1d738782b6fdd46b1ab73c
Danny Wamuya <dwamuya@gmail.com>
2023-01-16 13:22:01 +0300

    feat: use reducer

--- native-usereducer/src/App.tsx
+++ native-usereducer/src/App.tsx
@@ -1,34 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useReducer } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const NameList = () => {
+  const [state, dispatch] = useReducer<(state: any, action: any) => any>(
+    (state, action) => {
+      switch (action.type) {
+        case "SET_NAME":
+          return { ...state, name: action.payload };
+        case "ADD_NAME":
+          return { names: [...state.names, state.name], name: "" };
+        default:
+          break;
+      }
+    },
+    { name: "", names: [] }
+  );
+  return (
+    <>
+      <div>Name: {state.name}</div>
+      <div>
+        Names:
+        <ul>
+          {state.names.map((n: any, idx: any) => (
+            <li key={`${n}_${idx}`}>{n}</li>
+          ))}
+        </ul>
+      </div>
+      <input
+        type={"text"}
+        value={state.name}
+        onChange={(e) =>
+          dispatch({ type: "SET_NAME", payload: e.target.value })
+        }
+      />
+      <button onClick={() => dispatch({ type: "ADD_NAME" })}>Add Name</button>
+    </>
+  );
+};
+
+const UserForm = () => {
+  const [state, dispatch] = useReducer<(state: any, payload: any) => any>(
+    (state: any, action: any) => ({
+      ...state,
+      ...action,
+    }),
+    {
+      firstName: "",
+      lastName: "",
+    }
+  );
 
   return (
+    <>
+      <form>
+        <input
+          type={"text"}
+          value={state.firstName}
+          placeholder="Enter First Name"
+          onChange={(e) => dispatch({ firstName: e.target.value })}
+        />
+        <input
+          type={"text"}
+          value={state.lastName}
+          placeholder="Enter First Name"
+          onChange={(e) => dispatch({ lastName: e.target.value })}
+        />
+      </form>
+      <div>FirstName: {state.firstName}</div>
+      <div>LastName: {state.lastName}</div>
+    </>
+  );
+};
+
+function App() {
+  return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NameList />
+      <UserForm />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
