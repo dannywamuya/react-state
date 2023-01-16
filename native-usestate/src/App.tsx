f706f0ecf3df5dae4856f67ca29dd20208c947d6
Danny Wamuya <dwamuya@gmail.com>
2023-01-16 09:36:22 +0300

    feat: basic counter

--- native-usestate/src/App.tsx
+++ native-usestate/src/App.tsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
+function Counter() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return <button onClick={() => setCount(count + 1)}>Counter : {count}</button>;
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
